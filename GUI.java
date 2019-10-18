601eb15f6973ab2a5ff1d72a532384625e42d83c
agrawaladitya222 <55710891+agrawaladitya222@users.noreply.github.com>
2019-10-18 01:32:41 -0400

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -3,9 +3,8 @@ package longestsequence;
 import javax.swing.*;
 import BreezySwing.*;
 
-public class GUI extends GBFrame{
-	LongestSequence sender = new LongestSequence();
-	
+public class GUI extends GBFrame {
+
 	public static void main(String[] args) {
 		JFrame frm = new GUI();
 		frm.setTitle("Longest Non-Decreasing Sequence");
@@ -14,55 +13,43 @@ public class GUI extends GBFrame{
 	}
 
 	public GUI() {
-		
+
 	}
-	
-	JLabel inputlabel = addLabel("Input a number into the sequence.",1,1,1,1);
-	IntegerField inputfield = addIntegerField(0,1,2,1,1);
-	JTextArea outputarea = addTextArea("",3,1,1,1);
-	JButton inputbutton = addButton("Input Number",2,1,1,1);
-	JButton outputbutton = addButton("Output", 2,2,1,1);
-	JButton resetbutton = addButton("Reset",2,3,1,1);
-	
+
+	JLabel inputlabel = addLabel("Input a number into the sequence.", 1, 1, 1, 1);
+	IntegerField inputfield = addIntegerField(0, 1, 2, 1, 1);
+	JTextArea outputarea = addTextArea("", 3, 1, 1, 1);
+	JButton inputbutton = addButton("Input Number", 2, 1, 1, 1);
+	JButton outputbutton = addButton("Output", 2, 2, 1, 1);
+	JButton resetbutton = addButton("Reset", 2, 3, 1, 1);
+	LongestSequence sender = new LongestSequence();
+	JButton endbutton = addButton("Exit", 4, 2, 1, 1);
+
 	public void buttonClicked(JButton button) {
 		if (button == inputbutton) {
-			if(inputfield.isValidNumber())
+			if (inputfield.isValidNumber() && !sender.maxinputs()) {
 				sender.setInput(inputfield.getNumber());
-			else
-				messageBox("Invalid Input. Please try again.");
+			} else {
+				messageBox("Invalid input or max inputs reached. Please try again.");
+			}
 			inputfield.requestFocus();
 		}
-		if(button == outputbutton) {
-			sender.findSequence();
-			System.out.println(sender.getOutput());
-			
-			
+		if (button == outputbutton) {
+			sender.DetermineLongest();
+			sender.buildOutput();
+			outputarea.setText(sender.getOutput());
+			inputfield.requestFocus();
 		}
-		if(button == resetbutton) {
-			
+		if (button == resetbutton) {
+			sender.reset();
+			outputarea.setText("");
+			inputfield.setNumber(0);
+			inputfield.requestFocus();
 		}
-		
-		
-		
-		
-		
-		
+		if (button == endbutton) {
+			System.exit(0);
+		}
+
 	}
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
--- LongestSequence.java
+++ LongestSequence.java
@@ -1,71 +1,102 @@
 package longestsequence;
 
 public class LongestSequence {
-	private int[] input = new int[25];
-	private int inputindex=0;
-	private String output = "The longest nondecreasing sequence(s) out of the numbers you input:\n";
-	
-	public void setInput(int a) {
-		input[inputindex] = a;
-		inputindex = inputindex + 1;
-	}
-	
-	public void tester() {
-		for(int counter=0; counter<=24; counter++) {
-			System.out.println(input[counter]);
-		}
-	}
-	
-	public void resetInputs() {
-		for(int counter=0;counter<=24; counter++) {
-			input[counter]=0;
-		}
-		inputindex=0;
-	}
-	
-	public void findSequence() {
-		int numberbefore=input[0];
-		int longsequencelength=0,testsequencelength=0;
-		int firstnumberindex=9999;
-		
-		//determine longsequence length
-		for(int counter=0; counter<=24; counter++) {
-			if(input[counter] >= numberbefore) {
-				testsequencelength = testsequencelength+1;
-			}
-			else {
-				if(testsequencelength>=longsequencelength) {
-					longsequencelength = testsequencelength;
-					firstnumberindex = counter - longsequencelength;
-					//buildOutput(firstnumberindex, longsequencelength);
-				}
-				testsequencelength = 1;
-			}
-			numberbefore = input[counter];
-		}
-		
-		System.out.println(longsequencelength);
-		System.out.println(firstnumberindex);
-		
-		
-	}
-	
-	public void buildOutput(int firstnumber,int length) {
-		for(int counter=firstnumber; counter<(length + firstnumber); counter++) {
-			output = output + input[counter] + " ";
-		}
-		output = output + "\n";
-	}
-	
-	
-	public String getOutput() {
-		
-		return output;
-	}
-	
-	
-	
-	
-	
+	private int[][] input = new int[25][25];
+	private int[] templongest = new int[25];
+	private int inputindex = 0, inputlength = 0, longest = 0, inputbefore, totaluserinputs = 0;
+	private String output = "Longest non decreasing sequence(s):\n";
 
+	public boolean maxinputs() {
+
+		if (totaluserinputs == 25)
+			return true;
+		else
+			return false;
+
+	}
+
+	public LongestSequence() {
+		int lownum = -9999999;
+		for (int counter1 = 0; counter1 <= 24; counter1++) {
+			for (int counter2 = 0; counter2 <= 24; counter2++) {
+				input[counter2][counter1] = lownum;
+			}
+			lownum = lownum - 1;
+		}
+	}
+
+	public void setInput(int a) {
+		if ((inputindex == 0) && (inputlength == 0)) {
+			// first user input v
+			input[inputindex][inputlength] = a;
+			inputbefore = a;
+			inputlength = inputlength + 1;
+		} else {
+			// everything after first user input v
+			if (a >= inputbefore) {
+				input[inputindex][inputlength] = a;
+				inputbefore = a;
+				inputlength = inputlength + 1;
+			} else {
+				inputlength = 0;
+				inputindex = inputindex + 1;
+				input[inputindex][inputlength] = a;
+				inputbefore = a;
+				inputlength = inputlength + 1;
+			}
+		}
+		totaluserinputs = totaluserinputs + 1;
+	}
+
+	public void DetermineLongest() {
+
+		for (int asdf = 0; asdf <= 24; asdf++) {
+			templongest[asdf] = 0;
+		}
+
+		for (int sequencenumber = 0; sequencenumber < totaluserinputs; sequencenumber++) {
+			int numberbefore = input[sequencenumber][0];
+			for (int sequencelength = 0; sequencelength < totaluserinputs; sequencelength++) {
+				if (input[sequencenumber][sequencelength] >= numberbefore) {
+					templongest[sequencenumber] = templongest[sequencenumber] + 1;
+				}
+				numberbefore = input[sequencenumber][sequencelength];
+			}
+			if (templongest[sequencenumber] > longest)
+				longest = templongest[sequencenumber];
+		}
+	}
+
+	public void buildOutput() {
+		output = "Longest non decreasing sequence(s):\n";
+		if (longest > 1) {
+			for (int counter = 0; counter <= 24; counter++) {
+				if (templongest[counter] == longest) {
+					for (int counter2 = 0; counter2 <= (longest - 1); counter2++) {
+						output = output + input[counter][counter2] + " ";
+					}
+					output = output + "\n";
+				}
+			}
+		} else {
+			output = output + "There were no non decreasing sequences";
+		}
+
+	}
+
+	public String getOutput() {
+		return (output);
+	}
+
+	public void reset() {
+		inputindex = inputlength = longest = totaluserinputs = 0;
+		output = "Longest non decreasing sequence(s):\n";
+		int lownum = -9999999;
+		for (int counter1 = 0; counter1 <= 24; counter1++) {
+			for (int counter2 = 0; counter2 <= 24; counter2++) {
+				input[counter2][counter1] = lownum;
+			}
+			lownum = lownum - 1;
+		}
+	}
 }
